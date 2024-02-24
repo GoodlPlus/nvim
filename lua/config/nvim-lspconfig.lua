aa965d6d38c5123cd56a4bd9c962c20e89530988
liangjiahao.24 <liangjiahao.24@bytedance.com>
2024-02-25 01:24:20 +0800

    replace diagnostic sign

--- lua/config/nvim-lspconfig.lua
+++ lua/config/nvim-lspconfig.lua
@@ -1,3 +1,11 @@
+local function replace_diagnostic_sign()
+    local signs = { Error = "", Warn = "", Hint = "󰌵", Info = "" }
+    for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+end
+
 return {
     "neovim/nvim-lspconfig",
     lazy = true,
@@ -7,6 +15,7 @@ return {
         for server, option in pairs(opts) do
             configs[server].setup(option)
         end
+        replace_diagnostic_sign()
         -- Use LspAttach autocommand to only map the following keys
         -- after the language server attaches to the current buffer
         vim.api.nvim_create_autocmd("LspAttach", {
@@ -14,21 +23,21 @@ return {
             callback = function(ev)
                 -- Buffer local mappings.
                 -- See `:help vim.lsp.*` for documentation on any of the below functions
-                local opts = { buffer = ev.buf }
+                local keymap_opts = { buffer = ev.buf }
                 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-                vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-                vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, keymap_opts)
+                vim.keymap.set("n", "]d", vim.diagnostic.goto_next, keymap_opts)
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, keymap_opts)
                 -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                vim.keymap.set("n", "gd", "<Cmd>Telescope lsp_definitions initial_mode=normal<CR>", opts)
+                vim.keymap.set("n", "gd", "<Cmd>Telescope lsp_definitions initial_mode=normal<CR>", keymap_opts)
                 -- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-                vim.keymap.set("n", "gi", "<Cmd>Telescope lsp_implementations initial_mode=normal<CR>", opts)
+                vim.keymap.set("n", "gi", "<Cmd>Telescope lsp_implementations initial_mode=normal<CR>", keymap_opts)
                 -- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-                vim.keymap.set("n", "gr", "<Cmd>Telescope lsp_references initial_mode=normal<CR>", opts)
-                vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+                vim.keymap.set("n", "gr", "<Cmd>Telescope lsp_references initial_mode=normal<CR>", keymap_opts)
+                vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, keymap_opts)
                 -- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
-                vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
-                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, keymap_opts)
+                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, keymap_opts)
                 vim.diagnostic.disable()
             end,
         })
